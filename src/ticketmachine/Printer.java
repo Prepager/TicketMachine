022ecd978b512399735a4076f230776d6e1c165b
ZapsterStudios <andreas@zapsterstudios.com>
2018-03-08 12:52:12 +0100

    :sparkles: Readded ticket print

--- src/ticketmachine/Printer.java
+++ src/ticketmachine/Printer.java
@@ -8,6 +8,20 @@ public class Printer {
      * @param ticket
      */
     public void printTicket(Client client, Ticket ticket) {
-        //
+        // Store ticket data variables.
+        double price = ticket.getPrice();
+        double balance = client.getBalance();
+        
+        // Output ticket to console.
+        System.out.println("##########B##T#########");
+        System.out.println("# BlueJ Trafikselskab #");
+        System.out.println("#                     #");
+        System.out.println("#        Ticket       #");
+        System.out.println("#       " + price + " DKK.     #");
+        System.out.println("#                     #");
+        System.out.println("##########B##T#########");
+        System.out.println("You have " + balance + " DKK left");
+        System.out.println("##########B##T#########");
+        System.out.println();
     }
 }
